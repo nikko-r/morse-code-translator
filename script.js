911e7fe7b99ea31634955a1371028a4ee94a62e1
nikko <nikkorivas@outlook.com>
2022-07-11 17:09:20 +0100

    Commented out random code

--- script.js
+++ script.js
@@ -3,32 +3,29 @@ import translationArr from "./translations.js";
 const engBox = document.querySelector("#eng-box");
 const morseBox = document.querySelector("#morse-box");
 
-// //Function for getting pokemon entry data
-// const modifyPokemonEntries = (pokemonEntry) => {
-//     cardContainer.innerHTML += `
-//         <div class="card">
-//                 <p class="card__text">${pokemonEntry.eng} ${pokemonEntry.morse}</p>
-//             </div>
-//         </div>
-//         `;
-// };
-
-//Loads all pokemon data on load
 translationArr.forEach((entry) => {
     console.log(entry);
 });
 
-const filterPokemonByNameEntries = () => {
-    cardContainer.innerHTML = ``;
-    pokemonArray.forEach((pokemonEntry) => {
-        if (pokemonEntry.name.includes(searchBox.value)) {
-            modifyPokemonEntries(pokemonEntry);
-        }
-    });
-};
+// const getMorseCode = () => {
+//     //morseBox.value +=;
+//     translationArr.forEach((entry) => {
+//         if (entry.eng.includes(searchBox.value)) {
+//             modifyEntries(entry);
+//         }
+//         for (let i = 0; i < morseBox.value.length; i++) {
+//             str[i]);
+//             translationArr.forEach((entry) => {
+//                 if (entry.eng.includes(searchBox.value)) {
+//     });
+// };
 
-//Event listener for pokemon name filtering
-searchBox.addEventListener("change", filterPokemonByNameEntries);
+// for (let i = 0; i < engBox.value.length; i++) {
+//     str[i]);
+//     translationArr.forEach((entry) => {
+//         if (entry.eng.includes(searchBox.value)) {
+
+// engBox.addEventListener("change", getMorseCode);
 
 //psuedo
 
